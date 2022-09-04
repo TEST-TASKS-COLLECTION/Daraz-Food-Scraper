e075ca671667f5601f820af26843fc67dce31f9c
mikeyy <adhikari.shrish1@gmail.com>
2022-09-04 19:27:09 +0545

    Added pipelines

--- codes/scraper.py
+++ codes/scraper.py
@@ -1,4 +1,5 @@
 from unicodedata import name
+import os
 import requests
 from bs4 import BeautifulSoup as bs
 import json
@@ -18,7 +19,7 @@ def get_product():
     args = parser.parse_args()
     return args.prod
     
-def unit_parser(item):
+def unit_parser(item, standarize=False):
     patt = re.compile(PATTERN)
     d = patt.findall(item['name'].lower())
     if not d:
@@ -29,15 +30,26 @@ def unit_parser(item):
             "amount": d[0][1],
             "unit": d[0][2].strip(),
         }
+        if not standarize:
+            return return_dict
         return standarize_unit(return_dict)
+
+def save_data(product, path, data):
+    with open(path, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(['Product', 'Quantity', "Unit"])
+        for item in data:
+            print(item)
+            writer.writerow([item["name"].strip(), item["amount"].strip(), item["unit"].strip()])
 
 
 def standarize_unit(item):
+    print(item)
     if item['unit'] in ['l', 'ltr']:
-        item['amount'] = str(int(item['amount']) * 1000)
+        item['amount'] = str(float(item['amount']) * 1000)
         item['unit'] = "ml"
     elif item['unit'] in ['kg']:
-        item['amount'] = str(int(item['amount']) * 1000)
+        item['amount'] = str(float(item['amount']) * 1000)
         item['unit'] = "gm"
     
     return item
@@ -53,30 +65,56 @@ class DarazScraper:
         return res['mods']['listItems']
     
     
-    def save_data(self, data):
-        with open(f"data/{self.query}.csv", "w") as f:
-            writer = csv.writer(f)
-            writer.writerow(['Product', 'Quantity', "Unit"])
-            for item in data:
-                print(item)
-                writer.writerow([item["name"].strip(), item["amount"].strip(), item["unit"].strip()])
-    
-    def get_data(self):
+
+    def get_data(self, standarize=True):
         """
         Returns:
             items (list): list of tuples containing the item name and its quantity  
         """
         data = self.get_request_json()
         
-        items = [unit_parser(i) for i in data if unit_parser(i)]
+        items = [unit_parser(i, standarize) for i in data if unit_parser(i)]
         # items = [patt.findall(i['name'].lower()) for i in data if patt.findall(i['name'])]
         print(items)
-        self.save_data(items)
+        path = f"data/{self.query}.csv"
+        save_data(items, path)
         return items
             # print(item.name)
         # return soup.select_one(".title--wFj93")
 
+def check_product_exists(product):
+    if f"{product}.csv" in os.listdir('data'):
+        print("PRODUCT ALREADY EXISTS NO NEED TO SCRAP")
+        return True
+
+    return False
+
+def scrape(product):
+    if not check_product_exists(product):
+        DarazScraper(product, False)
+
+def process(product):
+    items = []
+    with open(f"data/{product}.csv") as f:
+        next(f)
+        r = csv.reader(f)
+        for i in r:
+            items.append({
+                "name": i[0].strip(),
+                "amount": i[1],
+                "unit": i[2].strip()
+            })
+    items = [standarize_unit(item) for item in items]
+    
+    path = f"data/{product}_std.csv"
+    save_data(product, path, items)
+
+def run(product, standarize=True):
+        scrape(product)
+        process(product)
 
 if __name__ == "__main__":
     product = get_product()
-    data = DarazScraper(product).get_data()
+    # scrape(product)
+    # process(product)
+    run(product)
